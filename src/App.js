2fa6b0fd3a6651219ce93ea8dd4e2cd9a13fd259
Luigi594 <luishnlacho@gmail.com>
2022-05-20 17:17:13 -0600

    Initializing the Slack clone, implementing styled components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Header />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
